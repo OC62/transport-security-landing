c35814876f9159e569a654bc8de589f37d9b4737
Petr Matvienko <oc611164@gmail.com>
2025-07-28 16:44:09 +0300

    Fix base path to relative paths

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/transport-security-landing/',
+  base: './',  // Относительные пути вместо абсолютных
   plugins: [react()],
   build: {
     rollupOptions: {
