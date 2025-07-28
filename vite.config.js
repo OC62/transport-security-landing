b2b57a79e38340b3fdbc8b17c1a28b59176fc598
Petr Matvienko <oc611164@gmail.com>
2025-07-28 16:45:40 +0300

    Fix asset paths and favicon

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',  // Относительные пути вместо абсолютных
+  base: './',
   plugins: [react()],
   build: {
     rollupOptions: {
